a89fc1ef749c8a8736c47862d9ace8c553fea174
778116@cognizant.com <ammar.gangardiwala@cognizant.com>
2023-03-04 19:31:15 -0600

    jkfdj

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-app';
-}
+  title = 'Angular App';
+  response = "No data loaded, yet";
+
+  constructor(private http: HttpClient) {
+    this.http.get('http://localhost:8090/weatherforecast',
+      { responseType: 'text' }).subscribe((response: any) => {
+
+        console.log(response);
+        this.response = response;
+      });
+  }
+  //https://api.publicapis.org/entries
+};
